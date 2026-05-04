474ace7cbed9c2b5c2714f14735a36b724b498bb
J4CKVVH173 <i78901234567890@gmail.com>
2026-05-04 21:59:29 +0300

    new fix

--- raspberry-pi-backend/scripts/jellyfin/get_logs.py
+++ raspberry-pi-backend/scripts/jellyfin/get_logs.py
@@ -7,7 +7,7 @@ import subprocess
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent.parent))
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 import config
 
 
--- raspberry-pi-backend/scripts/yandex/check_status.py
+++ raspberry-pi-backend/scripts/yandex/check_status.py
@@ -4,7 +4,7 @@ import os
 
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent.parent))
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 import config
 
 INSTANCE_ID = os.environ["YANDEX_INSTANCE_ID"]
